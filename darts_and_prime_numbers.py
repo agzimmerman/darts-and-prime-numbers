0e95dc31450920eb7cceb5f51fd51d0af6fffdb3
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2017-07-26 21:12:33 +0200

    Report min and max. Tabulate outputs. Added sample output in header

--- darts_and_prime_numbers.py
+++ darts_and_prime_numbers.py
@@ -12,9 +12,21 @@ But for now the question is: Is this actually unusual? If someone were to place 
 Unless there is a mistake in this script (which is quite likely), the expected value is six, but the variance is four,
 so the observed count on a standard dart board is within two standard deviations, and therefore not so remarkable.
 
+The minimums and maximums are quite interesting. Here is a sample output:
 
+ Samples   Mean    Var Min Max
+     100  6.090  3.982   2  12
+    1000  6.035  4.194   0  14
+   10000  5.918  4.040   0  14
+  100000  5.896  4.023   0  16
+ 1000000  5.894  4.028   0  16
 
-Tested on Python 2.7.12 :: Anaconda custom (64-bit)
+ 
+Tested on...
+
+    Python 2.7.12 :: Anaconda custom (64-bit)
+    Python 2.7.6
+    
 '''
 
 import random
@@ -41,14 +53,16 @@ numbers = range(1, section_count + 1)
 
 ''' We employ the Monte Carlo integration method to estimate the expected value of the number of ajacted pairs that sum to a prime number.
 '''
-large_number = 10000
+large_number = 1000000
 
-report_at = (100, 1000, 10000)
+report_at = (100, 1000, 10000, 100000, 1000000)
 
 prime_counts = []
 
 
 ''' Run the Monte Carlo loop '''
+print(" Samples   Mean    Var Min Max")
+
 for sample in range(1, large_number + 1):
 
     random.shuffle(numbers) # This randomizes the order of the list of numbers, modifying the list in place
@@ -71,5 +85,5 @@ for sample in range(1, large_number + 1):
     ''' Update the expected value, and report it regularly to check for convergence '''
     if sample in report_at:
     
-        print('With a sample size of '+str(sample)+', the expected value is '+str(numpy.mean(prime_counts))+' and the variance is '+str(numpy.var(prime_counts)))
+        print " %7d %6.3f %6.3f %3d %3d" % (sample, numpy.mean(prime_counts), numpy.var(prime_counts), numpy.min(prime_counts), numpy.max(prime_counts))
         
